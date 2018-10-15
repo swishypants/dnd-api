2a7f29b3cc1b9df2065e14bd2b102f15ca28cc5d
swishypants <shamuflute@gmail.com>
2018-10-14 20:21:36 -0700

    update class info

--- character-service/src/main/java/domain/Character.java
+++ character-service/src/main/java/domain/Character.java
@@ -3,9 +3,19 @@ package domain;
 import domain.enums.CharacterClass;
 import domain.enums.CharacterRace;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Map;
 
 @Data
 public class Character {
   CharacterRace characterRace;
-  CharacterClass characterClass;
+  ClassDetails classDetails;
+
+  @Getter
+  @Setter
+  private class ClassDetails {
+    Map<CharacterClass, Integer> classMap;
+  }
 }
