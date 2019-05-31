2e303b03dce42b31b0fe771d9ebde8927a701c33
swishypants <shamuflute@gmail.com>
2019-05-30 22:56:42 -0700

    Refactor application module into a package under character-manager

--- character-manager/src/main/java/api/CharacterResource.java
+++ character-manager/src/main/java/api/CharacterResource.java
@@ -1,10 +1,10 @@
 package api;
 
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Hello {
+public class CharacterResource {
 
   @RequestMapping("/")
   public String index() {
